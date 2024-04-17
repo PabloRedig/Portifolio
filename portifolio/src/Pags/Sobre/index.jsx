10ef19ee6009e62fc4315204a51bb44e294e4c37
Pablo Redig <pabloleaoredig@hotmail.com>
2024-04-17 17:50:47 -0300

    Criado o Css da Pag Sobre

--- portifolio/src/Pags/Sobre/index.jsx
+++ portifolio/src/Pags/Sobre/index.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import ImgPerfil from './Img/FotoPerfil.svg'
+import style from './index.module.css'
 
 export default function Sobre() {
   return (
     <>
-      <div>
-        <h1>PAGINA SOBRE</h1>
-        <section>
+      <div className={style.BlocoPag}>
+        <span className={style.TituloPag}>SOBRE</span>
+      </div>
+      <div className={style.container}>
+        <section className={style.BlocoFoto}>
           <img src={ImgPerfil} alt="Foto Perfil" />
         </section>
-        <section>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, tenetur minima culpa consequatur incidunt distinctio natus nobis omnis tempora mollitia cum sunt quaerat nulla asperiores inventore magnam eos fugiat repudiandae.</p>
+        <section className={style.BlocoTexto}>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit,
+            tenetur minima culpa consequatur incidunt distinctio natus nobis
+            omnis tempora mollitia cum sunt quaerat nulla asperiores inventore
+            magnam eos fugiat repudiandae.</p>
         </section>
       </div>
     </>
